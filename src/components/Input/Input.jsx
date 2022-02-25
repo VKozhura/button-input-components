6e707a6d56acf6b923fd3ef46e504da2659335fc
VKozhura <kozhura_v@mail.ru>
2022-02-25 12:28:07 +0300

    lj,fdbkf

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,12 +1,13 @@
 import classNames from "classnames";
 import styles from "./Input.module.css";
 
-export const Input = ({ error, disabled, helperText, value, size }) => {
+export const Input = ({ error, disabled, helperText, value, size, fullwidth, multiline, row }) => {
 	const inputClass = classNames({
 		[styles.input]: true,
 		[styles.inputError]: error,
 		[styles.inputSmall]: size === "sm",
 		[styles.inputMedium]: size === "md",
+		[styles.fullwidth]: fullwidth,
 	});
 	const smallClass = classNames({
 		[styles.small]: true,
@@ -21,16 +22,32 @@ export const Input = ({ error, disabled, helperText, value, size }) => {
 				{helperText ? ` helperText='${helperText}' ` : ""}
 				{value ? ` value='${value}' ` : ""}
 				{size ? ` size='${size}' ` : ""}
+				{fullwidth ? ` fullwidth ` : ""}
+				{multiline ? ` multiline row='${row}' ` : ""}
 				/&#8250;
 			</p>
-			<input
-				className={inputClass}
-				id=""
-				type="text"
-				placeholder="Placeholder"
-				disabled={disabled}
-				value={value}
-			/>
+			{!multiline && (
+				<input
+					className={inputClass}
+					id=""
+					type="text"
+					placeholder="Placeholder"
+					disabled={disabled}
+					value={value}
+				/>
+			)}
+			{multiline && (
+				<textarea
+					rows={row}
+					className={inputClass}
+					id=""
+					type="text"
+					placeholder="Placeholder"
+					disabled={disabled}
+					value={value}
+				/>
+			)}
+
 			<label htmlFor="">Label</label>
 			<small className={smallClass}>{helperText}</small>
 		</div>
